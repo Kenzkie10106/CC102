ab71129095dfb03dab50f6e45720cee52338ea20
ken rafa <razelkenr@gmail.com>
2024-10-03 10:50:18 +0800

    Add files via upload

--- 02BasicOutput.cpp
+++ 02BasicOutput.cpp
@@ -1,13 +1,13 @@
 #include <iostream>
 using namespace std;
 
- int main ();
+
+ int main ()
  {
     cout << "Our Lady of Lourdes College Foundation" <<endl;
 	cout << "Computer of Science Study" <<endl;
 	cout << "BSIT FIRST YEAR" <<endl;
-	cOUt << "Razel Ken Rafa" <<endl;
-	cout << "BRGY P-2 DALNAC PARACALE CAM NORTE" <<
-	
-return 0;
+	cout << "Razel Ken Rafa" <<endl;
+	cout << "BRGY P-2 DALNAC PARACALE CAM NORTE" <<endl;
+	return 0;
  }
