df885a4366afc39f741d6e5f68799a2f7e11e9d5
ken rafa <razelkenr@gmail.com>
2024-10-08 10:31:05 +0800

    Add files via upload

---
+++ 04BasicInput.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+
+ int main ()
+ {
+	string nickname,course,year,school;
+	
+	cout << "Nickname" << endl;
+	cin >> nickname;
+	cout <<"Course year level" <<endl;
+	cin >> course;
+	cout << "School" << endl;
+	cin >> school;
+	cout << "wow congrats" << nickname << "!" << course <<  "<< is a nice course.And you are studying in "<< school << "  which is one of the center of excellence in tertiary education" << endl;
+	
+	return 0;
+ }
